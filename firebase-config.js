783de6259c60768cc3e6f4f0b383de36312a956e
gdezayas <gdzayas@gmail.com>
2023-01-17 22:06:23 -0500

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDc0cKaCx6wep563sIvNuYSJEtOKo_q5Bc",
+  authDomain: "consultoria-8e60b.firebaseapp.com",
+  projectId: "consultoria-8e60b",
+  storageBucket: "consultoria-8e60b.appspot.com",
+  messagingSenderId: "415652395128",
+  appId: "1:415652395128:web:bbbd437fcec3d8ee0f958d"
+};
+
